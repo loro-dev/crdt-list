7f2a440ff9d88bd54dfd72c83b0d97cca808f6b9
Zixuan Chen <remch183@outlook.com>
2022-09-27 17:07:57 +0800

    test: update fuzz

--- fuzz/fuzz_targets/woot.rs
+++ fuzz/fuzz_targets/woot.rs
@@ -5,4 +5,4 @@ use crdt_woot::{crdt::OpSet, test, woot, woot_dumb_impl::WootImpl};
 use libfuzzer_sys::fuzz_target;
 use rand::Rng;
 
-fuzz_target!(|data: (usize, usize, u64)| { test::test::<WootImpl>(data.2, 2, data.1 % 1000 + 1) });
+fuzz_target!(|data: (usize, usize, u64)| { test::test::<WootImpl>(data.2, 10, data.1 % 1000 + 1) });
